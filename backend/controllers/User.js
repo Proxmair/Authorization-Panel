7176f7849007901e00fd5d9607ee6e2d31b67d88
Proxmair <73277454+Proxmair@users.noreply.github.com>
2023-04-17 23:08:37 +0500

    Update User.js

--- backend/controllers/User.js
+++ backend/controllers/User.js
@@ -166,7 +166,7 @@ export const forgotPassword = async (req, res) => {
     }
     const resetToken = user.getResetPasswordToken();
     await user.save({ validateBeforeSave: false });
-    const resetPasswordUrl = `${process.env.FRONTEND_URL}/password/reset/${resetToken}`;
+    const resetPasswordUrl = `https://umair-authentication-panel.vercel.app/password/reset/${resetToken}`;
     const message = `Your password reset token is :- \n\n ${resetPasswordUrl} \n\nIf you have not requested this email then, please ignore it.`;
     try {
       await sendEmail(user.email, "Recovery Password Mail", message);
